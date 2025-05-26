f8af857d75e5cc598051298cf498c2658b2dabf9
TAAT <taat@TAATnoMacBook-Pro.local>
2025-05-26 23:47:46 +0900

    Initial commit: Swift SolarSystem project with Vision Pro support

---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
